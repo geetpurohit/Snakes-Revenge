4ac7808f5cead2495e7e1752bc151a4cf0e45ee7
geetpurohit <geetp40@gmail.com>
2021-11-29 03:53:49 -0500

    Fixed poisoned food color flickering

---
+++ SuperFood.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+screen_width = 720
+screen_height = 720
+
+gridsize = 20
+grid_width = screen_width/gridsize
+grid_height = screen_height/gridsize
+
+up = (0,-1)
+down = (0,1)
+left = (-1,0)
+right = (1,0)
+
+
+class SuperFood():
+    def __init__(self):
+        self.position = (0,0)
+        self.color = (223, 20, 22)
+        self.randomize_position()
+            
+            
+    def randomize_position(self):
+        self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
+
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, (93, 216, 228), r, 1)
